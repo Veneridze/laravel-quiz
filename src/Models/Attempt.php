10c04035b40e221d1b8d63db6fe4a5d5593e8de7
Veneridze <viacheslav.kopaev@yandex.ru>
2024-10-25 09:46:48 +0300

    Update Attempt.php

--- src/Models/Attempt.php
+++ src/Models/Attempt.php
@@ -19,12 +19,12 @@ class Attempt extends Model {
     }
 
     public function getTimeAttribute(): int | null {
-        return !$this->isActive ? Carbon::parse($this->finished_at)->diffInSeconds(Carbon::parse($this->created_at)) : null;
+        return !$this->isActive ? Carbon::parse($this->ended_at)->diffInSeconds(Carbon::parse($this->created_at)) : null;
     }
 
     public function getisActiveAttribute() {
         
-        return is_null($this->finished_at) || (is_null($this->quiz->time) || Carbon::parse($this->finished_at)->diffInSeconds() < $this->quiz->time);
+        return is_null($this->ended_at) || (is_null($this->quiz->time) || Carbon::parse($this->ended_at)->diffInSeconds() < $this->quiz->time);
     }
 
     public function finish() {
